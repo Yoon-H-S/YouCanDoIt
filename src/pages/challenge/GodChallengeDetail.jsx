8e7f7625b03da70d8a94384db81d40ad5a2e6d91
201qu03 <201qu03@g.shingu.ac.kr>
2023-06-07 16:59:00 +0900

    갓생 챌린지 상세보기

--- src/pages/challenge/GodChallengeDetail.jsx
+++ src/pages/challenge/GodChallengeDetail.jsx
@@ -12,6 +12,22 @@ function GodChallengeDetail(props) {
 				<FontAwesomeIcon icon={faChevronLeft} />
 				<span>갓생 챌린지</span>
 			</Title>
+			<GodDetail>
+				<Picture />
+				<GodTitle> (N주) N보 걷기 </GodTitle>
+				<GodContent> 습관 기르기 프로젝트! <br /> N주 동안 걷고 습관 기르자. <br /> 습과 기르면서 N보 이상 걸으면서 운동하면 건강도 관리하고 일석이조 <br /> 지인과 함께 챌린지 그룹을 만들고 랭킹에 참여하세요! </GodContent>
+				<Keword>
+					<OneKey> 매일 </OneKey>
+					<TwoKey> N주 동안 </TwoKey>
+					<ThreeKey> 꾸준히 </ThreeKey>
+				</Keword>
+				<Button
+					width = "63px"
+					height = "23px"
+					title = "생성하기"
+					color = "#0077E4"
+				/>
+			</GodDetail>
 		</Wrapper>
 	);
 }
@@ -26,18 +42,16 @@ const Wrapper = styled.div`
 	align-items: center;
 	width: 100%;
 	height: 100%;
-	overflow: hidden;
 `;
 
 // 그룹 생성하기 타이틀 영역
 const Title = styled.div`
 	position: relative;
-	top: 5px;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	width: 100%;
-	height: 50px;
+	height: 65px;
 	z-index: 2;
 
 	& > span {
@@ -53,4 +67,77 @@ const Title = styled.div`
 		left: 45px;
 		color: black;
 	}
+`;
+
+const GodDetail = styled.div`
+	position: relative;
+	flex-direction: column;
+	align-items: center;
+	width: 465.5px;
+	height: 332px;
+	border-radius: 10px;
+
+	& > Button {
+		margin-top: 11px;
+		margin-left: auto;
+	}
+`;
+
+const Picture = styled.div`
+	width: 465.5px;
+	height: 120px;
+	border: 1px solid #B1B1B1;
+	border-radius: 10px;
+	margin-bottom: 15px;
+`;
+
+const GodTitle = styled.div`
+	font-size: 14px;
+	font-weight: bold;
+	padding-bottom: 15px;
+	border-bottom: 1px solid #B1B1B1;
+`;
+
+const GodContent = styled.div`
+	font-size: 12px;
+	padding: 10px 10px;
+	border-bottom: 1px solid #B1B1B1;
+`;
+
+const Keword = styled.div`
+	display: flex;
+	font-size: 11px;
+	margin-top: 10px;
+`;
+
+const OneKey = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 37px;
+	height: 24px;
+	border-radius: 5px;
+	background-color: #D9D9D9;
+	margin-right: 8px;
+`;
+
+const TwoKey = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 58px;
+	height: 24px;
+	border-radius: 5px;
+	background-color: #D9D9D9;
+	margin-right: 8px;
+`;
+
+const ThreeKey = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 47px;
+	height: 24px;
+	border-radius: 5px;
+	background-color: #D9D9D9;
 `;
