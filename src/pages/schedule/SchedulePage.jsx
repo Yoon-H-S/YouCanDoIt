62d5f2bff3d92b6a021ffa5be6f423a381ce14d5
song22ee <ann7335@naver.com>
2023-09-01 15:38:03 +0900

    [프론트]가이드/스케줄 틀 제작

---
+++ src/pages/schedule/SchedulePage.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Page from 'pages/Page';
+import styled from 'styled-components';
+
+function SchedulePage(props) {
+	return (
+		<Page>
+			<LeftContent></LeftContent>
+			<MiddleLine />
+			<RightContent></RightContent>
+		</Page>
+	);
+}
+
+export default SchedulePage;
+
+/** 좌측 페이지 영역 */
+const LeftContent = styled.div`
+	position: relative;
+	width: 555.5px;
+	height: 422px;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+// 중간 라인
+const MiddleLine = styled.div`
+	width: 0px;
+	height: 422px;
+	border-left: 1px solid black;
+	margin-left: 43px;
+	margin-right: 43px;
+`;
+
+// 우측 페이지 영역
+const RightContent = styled.div`
+	position: relative;
+	width: 555.5px;
+	height: 422px;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
